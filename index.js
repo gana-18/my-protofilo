42046b09dd60e44582710ce7c5ea75490ee5bdb5
Gana Sai Teja <velichetiganasaiteja@gmail.com>
2022-08-19 16:00:56 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function myFunction() {
+/*function myFunction() {
     var x = document.getElementById("myheader");
     if (x.className === "header") {
       x.className += " responsive";
     } else {
       x.className = "header";
     }
-  };
+  };*/
+  const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".sections");
+
+navToggle.addEventListener("click", function () {
+  // console.log(links.classList);
+  // console.log(links.classList.contains("random"));
+  // console.log(links.classList.contains("links"));
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else {
+  //   links.classList.add("show-links");
+  // }
+  links.classList.toggle("show-links");
+});
 const submitBtn = document.querySelector(".btn");
 const firstCard = document.querySelector("#card1");
 const secondCard = document.querySelector("#card2");
